c3a824c3ad74bc18aecf2ecbbcb0911f0bdc9a4d
kenkl <kenkl@mac.com>
2019-12-22 23:20:52 -0500

    missed a change in pirthing1_off.php

--- pirthing1_off.php
+++ pirthing1_off.php
@@ -8,8 +8,6 @@ include 'functions.php';
 include 'pirthing1_vars.php';
 
 if(ison($tl1)) { #if it's not already on (this would be odd), let's not do anything.
-    # Are we going to honour this trigger? This probably should express a range of times; crossing over 00:00 is going to be A Thing.
-    #if(($nowtime >= $st && $nowtime <= 2359) || ($nowtime >= 0000 && $nowtime <= $et) ) { 
     if(activetime()) { 
         echo "{$nowtime} - Do.\n";
         if (checkState($tl1)) {  #we don't actually care about the state, just that it's there
