af255d1107506737ae5aa659574adf41171ecc62
kenkl <kenkl@mac.com>
2020-12-26 10:31:14 -0500

    change order of light groups for goodmorning

--- goodmorning.php
+++ goodmorning.php
@@ -4,8 +4,8 @@
 # "Good morning, houseplants. Yes, it's wake-up time."
 include 'functions.php';
 
-$output = `/usr/bin/env curl -s http://max.kenkl.org/lights/brhalf.php`;
 $output = `/usr/bin/env curl -s http://max.kenkl.org/lights/normal.php`;
+$output = `/usr/bin/env curl -s http://max.kenkl.org/lights/brhalf.php`;
 $output = `/usr/bin/env curl -s http://max.kenkl.org/lights/alldlonfullwarm.php`;
 
 # clear any dangling toggles that were in use...
